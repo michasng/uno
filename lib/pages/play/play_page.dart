d2825bc124d131dbba1e3a97df1cb3b9ae8fa62f
Micha Sengotta <micha.sengotta@tngtech.com>
2025-12-19 07:39:14 +0100

    Replace counter app by an empty page

---
+++ lib/pages/play/play_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PlayPage extends StatelessWidget {
+  const PlayPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('The game will be playable here')),
+    );
+  }
+}
